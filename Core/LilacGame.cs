6e1c634a7138ce2f111f47bbf9105eae62a9698f
flats <hiimhibiscus@gmail.com>
2024-03-04 15:42:25 -0500

    test

--- Core/LilacGame.cs
+++ Core/LilacGame.cs
@@ -2,41 +2,50 @@
 global using Microsoft.Xna.Framework.Graphics;
 global using Microsoft.Xna.Framework.Input;
 
-namespace Lilac {
-    public class LilacGame : Game {
+namespace Lilac.Core
+{
+    public class LilacGame : Game
+    {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public LilacGame() {
+        public LilacGame()
+        {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
-        protected override void Initialize() {
+        protected override void Initialize()
+        {
             base.Initialize();
         }
 
-        protected override void LoadContent() {
+        protected override void LoadContent()
+        {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
-        protected override void Update(GameTime gameTime) {
-            if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime) {
+        protected override void Draw(GameTime gameTime)
+        {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             base.Draw(gameTime);
         }
 
-        protected override void UnloadContent() {
+        protected override void UnloadContent()
+        {
             base.UnloadContent();
         }
 
-        protected override void Dispose(bool disposing) {
+        protected override void Dispose(bool disposing)
+        {
             base.Dispose(disposing);
         }
     }
